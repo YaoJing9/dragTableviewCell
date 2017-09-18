e52bf525d25b61662d85ab852c49a3d137bb2ebb
cherry <cherry@cherrydeiMac.local>
2017-09-18 15:04:31 +0800

    Initial Commit

---
+++ YJMoveCellTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YJMoveCellTableView
+//
+//  Created by cherry on 2017/9/18.
+//  Copyright © 2017年 YaoJing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YJMoveCellTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YJMoveCellTableView
+//
+//  Created by cherry on 2017/9/18.
+//  Copyright © 2017年 YaoJing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
